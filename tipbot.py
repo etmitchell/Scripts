82ce41c2344167fa2822d950593ed13b52752c76
etmitchell <framerate333@gmail.com>
2016-02-25 17:39:22 -0500

    Current state. Will repeat back to you if you send a message containing 'tipbot: '.

--- tipbot.py
+++ tipbot.py
@@ -10,17 +10,42 @@ import ConfigParser
 import os
 
 def slack():
-    config = ConfigParser.RawConfigParser()
-    config.read('settings.cfg')
-    token = config._sections['bot_keys']['tipbot']
-    sc = SlackClient(token)
+    pass
 
-    print sc.api_call("chat.postMessage", channel="#bottesting",
-                      text="You've been tipped!",
+def send_message(sc,keyword=None):
+    sc.api_call("chat.postMessage", channel="#bottesting",
+                      text= "DID SOMEONE SAY {0}?!?".format(keyword),
                       username='tipbot', icon_emoji=':doge:')
 
 def main():
-    slack()
+    config = ConfigParser.RawConfigParser()
+    config.read('settings.cfg')
+    token = config._sections['bot_keys']['testing']
+    sc = SlackClient(token)
+
+    if sc.rtm_connect():
+        while True:
+            print sc.rtm_read()
+            new_evts = sc.rtm_read()
+            for evt in new_evts:
+                print(evt)
+                if "type" in evt and evt["type"] == "message":
+                    if "text" in evt and "tipbot:" in evt["text"]:
+                        keyword = evt["text"].rsplit(':', 1)[1].encode('utf8')
+                        send_message(sc,keyword)
+
+            time.sleep(1)
+    else:
+        print "Connection Failed, invalid token?"
+
+    while True:
+        new_evts = sc.rtm_read()
+        for evt in new_evts:
+            print(evt)
+            if "type" in evt:
+                if evt["type"] == "message" and "text" in evt:
+                    message=evt["text"]
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
