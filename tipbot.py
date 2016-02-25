69ca22c65ae62962648f46cb5b8137d479fa139a
Evan M <framerate333@gmail.com>
2016-02-25 15:37:00 -0500

    Update tipbot.py

--- tipbot.py
+++ tipbot.py
@@ -10,23 +10,14 @@ import ConfigParser
 import os
 
 def slack():
-<<<<<<< HEAD
     config = ConfigParser.RawConfigParser()
     config.read('settings.cfg')
     token = config._sections['bot_keys']['tipbot']
     sc = SlackClient(token)
 
-    # print sc.api_call("chat.postMessage", channel="#bottesting",
-    #                   text="You've been tipped!",
-    #                   username='tipbot', icon_emoji=':doge:')
-=======
-    token = "get your own"
-    sc = SlackClient(token)
-
     print sc.api_call("chat.postMessage", channel="#bottesting",
                       text="You've been tipped!",
                       username='tipbot', icon_emoji=':doge:')
->>>>>>> 06b033380b2da8a74ed625672633a8b7126d562d
 
 def main():
     slack()
